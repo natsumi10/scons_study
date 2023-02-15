cd90f36bf4aa89eb84663cfa4d03766eae28783d
natsumi10 <natsumi.k1234@gmail.com>
2023-02-15 11:00:51 +0900

    Continue to work for scons training.

--- sConstruct_test/main.cpp
+++ sConstruct_test/main.cpp
@@ -3,6 +3,9 @@
 class Line
 {
 	public:
+	Line(float begin, float end) : begin(begin),end(end)
+	{}
+
 	float GetLength()
 	{
 		return end - begin;
@@ -16,5 +19,14 @@ class Line
 int main()
 {
 	std::cout << "This is main function!" << std::endl;
+
+	float begin = 1.0;
+	float end = 3.5;
+	Line line(begin, end);
+
+	float length = line.GetLength();
+
+	std::cout << "Length:" << length << std::endl;
+
 	return 0;
 }
