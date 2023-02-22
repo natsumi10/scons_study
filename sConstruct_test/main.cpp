a80e49eaaacac71e96e2e4976a5352cee4950a6c
natsumi10 <natsumi.k1234@gmail.com>
2023-02-22 16:54:51 +0900

    Create method for iterator test.

--- sConstruct_test/main.cpp
+++ sConstruct_test/main.cpp
@@ -4,11 +4,24 @@ using namespace std;
 #include "line.h"
 #include "mathLibrary.h"
 
-
-int main()
+int iterator_test()
 {
-	std::cout << "This is main function!" << std::endl;
+	std::cout << "\nStart iterator_test method " << std::endl;
+	int arr_test[5] = {0,1,2,3,4};
+	int num = sizeof(arr_test)/sizeof(*arr_test);
+	std::cout << "size is " << num << std::endl;
 
+	int i;
+	for ( i=0; i < num; i++){
+		std::cout << "i = " << i << std::endl;
+		std::cout << "num = " << arr_test[i] << std::endl;
+	}
+	return 0;
+}
+
+int get_length()
+{
+	std::cout << "\nStart get_length method " << std::endl;
 	float begin = 1.0;
 	float end = 3.5;
 	line_t line(begin, end);
@@ -17,6 +30,12 @@ int main()
 
 	std::cout << "Length:" << length << std::endl;
 
+	return 0;
+}
+
+int lib_test()
+{
+	std::cout << "\nStart lib_test method " << std::endl;
 	double a = 7.4;
 	double b = 20;
 
@@ -26,5 +45,18 @@ int main()
 
 	std::cout << "a + b = " << ans << std::endl;
 
+	return 0;
+}
+
+int main()
+{
+	std::cout << "\nThis is main function!" << std::endl;
+
+	lib_test();
+
+	get_length();
+
+	iterator_test();
+	
 	return 0;
 }
